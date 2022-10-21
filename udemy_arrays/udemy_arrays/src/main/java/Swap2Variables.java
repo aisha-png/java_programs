199fac23cfd3e88cc7733a43d5a97ce0a7e521ce
aisha-png <67974517+aisha-png@users.noreply.github.com>
2022-10-21 21:13:47 +0100

    swap 2 variables solution

---
+++ udemy_arrays/udemy_arrays/src/main/java/Swap2Variables.java
@@ -0,0 +1,16 @@
+
+public class Swap2Variables {
+    public static void main(String[] args) {
+        String a = "Love";
+        String b = "You";
+
+        System.out.println("Before swap: " + a + " " + b);
+
+        a += b;
+        b = a.substring(0, a.length() - b.length());
+        a = a.substring(b.length());
+        System.out.println("After swap: " + a + " " + b);
+    }
+
+
+}
