8b5c64ac2a783e1c2c86d45966ac38c97a0d52fe
aisha-png <67974517+aisha-png@users.noreply.github.com>
2022-10-21 21:26:10 +0100

    word count function not working yet

--- udemy_arrays/udemy_arrays/src/main/java/CountWordsInString.java
+++ udemy_arrays/udemy_arrays/src/main/java/CountWordsInString.java
@@ -1,11 +1,11 @@
 
 public class CountWordsInString {
-    public static int wordCount(String str){
+    public static int wordCount(String string){
         int count = 0;
 
-        char ch[] = new char[str.length()];
-        for(int i = 0; i < str.length(); i++){
-            ch[i] = str.charAt(i);
+        char ch[] = new char[string.length()];
+        for(int i = 0; i < string.length(); i++){
+            ch[i] = string.charAt(i);
             if( ((i>0) && (ch[i] != ' ') || ((ch[0] != ' ') && (i==0))) );
             count++;
         }
