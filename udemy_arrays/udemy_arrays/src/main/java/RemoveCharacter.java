7777837133f0430fed7938a47a9090a3c84cbbbe
aisha-png <67974517+aisha-png@users.noreply.github.com>
2022-10-21 21:22:37 +0100

    remove character in string by index

---
+++ udemy_arrays/udemy_arrays/src/main/java/RemoveCharacter.java
@@ -0,0 +1,13 @@
+
+public class RemoveCharacter {
+    public static String removeCharAt(String str, int index){
+        return str.substring(0, index) + str.substring(index + 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "Somalia is my country";
+        System.out.println(removeCharAt(str,3));
+    }
+
+
+}
