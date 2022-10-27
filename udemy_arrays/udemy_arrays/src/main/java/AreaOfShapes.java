693698e466ef8851cb866bef36e276e159254b42
aisha-png <67974517+aisha-png@users.noreply.github.com>
2022-10-27 13:43:30 +0100

    area of circle using a scanner

--- udemy_arrays/udemy_arrays/src/main/java/AreaOfShapes.java
+++ udemy_arrays/udemy_arrays/src/main/java/AreaOfShapes.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class AreaOfShapes {
+
+//    using a scanner
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        double r = sc.nextDouble();
+        double area = (22*r*r)/7;
+        System.out.println("The area of your circle is : " + area);
+    }
 }
